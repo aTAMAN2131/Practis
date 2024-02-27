52e3de04a8c7718211d63f95a432f90704898301
aTAMAN2131 <161508149+aTAMAN2131@users.noreply.github.com>
2024-02-27 18:40:56 +0000

    add main code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
+#include <array>
+
+using namespace std;
+
+double AxisLength(const array<double, 100>& array, int n) {
+    double min = array[0];
+    double max = array[0];
+
+    for (int i = 1; i < n; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    double axLen = max - min;
+
+    return axLen;
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+    int n;
+
+    cout << "Введіть кількість елементів у послідовності: ";
+    if (!(cin >> n) || n <= 0) {
+        cout << "Введіть правильне значення\n";
+        return 1;
+    }
+
+    array<double, 100> array;
+
+    cout << "Введіть елементи послідовності:\n";
+    for (int i = 0; i < n; i++) {
+        if (!(cin >> array[i])) {
+            cout << "Введіть правильне значення\n";
+
+            while (cin.get() != '\n');
+
+            return 1;
+        }
+    }
+
+    double length = AxisLength(array, n);
+
+    cout << "Найменша довжина числової осі: " << length << "\n";
+
+    return 0;
 }
