44e9e5a17fc80b16db828dd44380830b76858d41
aTAMAN2131 <161508149+aTAMAN2131@users.noreply.github.com>
2024-02-27 19:12:50 +0000

    add pon

--- .ccls-cache/@home@runner@Pip/main.cpp
+++ .ccls-cache/@home@runner@Pip/main.cpp
@@ -1,51 +1,35 @@
 #include <iostream>
 #include <array>
-
-using namespace std;
-
-double AxisLength(const array<double, 100>& array, int n) {
-    double min = array[0];
-    double max = array[0];
-
-    for (int i = 1; i < n; i++) {
-        if (array[i] < min) {
-            min = array[i];
-        }
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-
-    double axLen = max - min;
-
-    return axLen;
-}
-
 int main() {
-    int n;
-
-    cout << "Введіть кількість елементів у послідовності: ";
-    if (!(cin >> n) || n <= 0) {
-        cout << "Введіть правильне значення\n";
+    int N;
+    std::cout << "Введіть кількість елементів масиву: ";
+    std::cin >> N;
+    if (N <= 0) {
+        std::cout << "Кількість елементів масиву має бути більшою за 0." << std::endl;
         return 1;
     }
-
-    array<double, 100> array;
-
-    cout << "Введіть елементи послідовності:\n";
-    for (int i = 0; i < n; i++) {
-        if (!(cin >> array[i])) {
-            cout << "Введіть правильне значення\n";
-
-            while (cin.get() != '\n');
-
-            return 1;
+    std::array<int, 100> A; 
+    std::cout << "Введіть елементи масиву:\n";
+    for (int i = 0; i < N; i++) {
+        std::cin >> A[i];
+        if (A[i] <= 0) {
+            std::cout << "Елементи масиву повинні бути натуральними." << std::endl;
+            return 1; 
         }
     }
-
-    double length = AxisLength(array, n);
-
-    cout << "Найменша довжина числової осі: " << length << "\n";
-
+    int k;
+    std::cout << "Введіть число k: ";
+    std::cin >> k;
+    if (k <= 0) {
+        std::cout << "Число k має бути більше за 0." << std::endl;
+        return 1; 
+    }
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] % k == 0) {
+            sum += A[i];
+        }
+    }
+    std::cout << "Сума елементів, кратних " << k << ": " << sum << std::endl;
     return 0;
 }
